1e9d462c847967765fb4ea9a65a2aeb965350712
Alexandre Lunelli da Silva <ovelhaaa@gmail.com>
2026-04-06 07:53:50 -0300

    ui: add partial invalidation flow to PerformScreen

--- src/ui/screens/PerformScreen.h
+++ src/ui/screens/PerformScreen.h
@@ -22,11 +22,21 @@ public:
   UiScreenId id() const override { return UiScreenId::Perform; }
 
 private:
-  bool _dirty{true};
+  bool _fullDirty{true};
+  bool _hasFrame{false};
+  bool _ringsDirty{true};
+  bool _controlsDirty{true};
+  bool _trackStripDirty{true};
+  bool _bpmDirty{true};
+  bool _lastPlaying{false};
+  uint16_t _lastBpm{0};
+  uint8_t _lastActiveTrack{0};
+  bool _lastTrackMutes[TRACK_COUNT]{};
   UiButton _playButton{};
   UiButton _muteButton{};
   UiCard _statusCard{};
   UiChip _trackChips[TRACK_COUNT]{};
+  UiRect _ringsRect{};
   UiEuclideanRings _rings{};
 };
 
